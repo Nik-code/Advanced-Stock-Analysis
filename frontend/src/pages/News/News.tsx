f204affab1135aa768b407abb61f44b7b1faaf3e
Priyansh Kumar Singh <priyanshnikka@gmail.com>
2024-08-24 00:20:25 +0530

    Add placeholders for pages

---
+++ frontend/src/pages/News/News.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const News: React.FC = () => {
+  return (
+    <div>
+      <h1>News</h1>
+      {/* Add News content here */}
+    </div>
+  );
+};
+
+export default News;
---
+++ frontend/src/pages/StockFund/StockFund.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StockFund: React.FC = () => {
+  return (
+    <div>
+      <h1>StockFund</h1>
+      {/* Add StockFund content here */}
+    </div>
+  );
+};
+
+export default StockFund;
