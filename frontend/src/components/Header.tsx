4bf5e4449efb5d71303c67749daaf0fed010f4fa
Priyansh Kumar Singh <priyanshnikka@gmail.com>
2024-08-23 23:34:12 +0530

    Add placeholder Dashboard, Header components and missing CSS modules

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <input type="text" placeholder="Search for various stocks" className={styles.searchBar} />
+      <div className={styles.userInfo}>
+        {/* We'll add user info and icons here later */}
+        User Info
+      </div>
+    </header>
+  );
+};
+
+export default Header;
